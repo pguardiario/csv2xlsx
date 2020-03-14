e2f7ef9d968303c31f85fcfced690e5e2ea60737
P Guardiario <p.guardiario@gmail.com>
2020-03-14 11:33:49 +0800

    init

--- bin.js
+++ bin.js
@@ -6,6 +6,9 @@ const outputFile = process.argv[3] || filename.replace(/csv$/i, 'xlsx')
 
 var workbook = new Excel.Workbook();
 workbook.csv.readFile(filename)
-  .then(worksheet => {
+  .then(ws => {
+    for(let i=0; i<ws.actualColumnCount; i++){
+      ws.getColumn(i + 1).numFmt = '@'
+    }
     workbook.xlsx.writeFile(outputFile)
   });
