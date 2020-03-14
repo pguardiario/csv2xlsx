3455fde7fdfb8e2fd6c6e0c3823836107667a963
P Guardiario <p.guardiario@gmail.com>
2020-03-14 11:21:07 +0800

    init

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # csv2xlsx
 convert csv file to xlsx
+
+- npm install -g pguardiario/csv2xlsx
+- Usage: csv2xlsx csvfile [xlsxfile]
---
+++ bin.js
@@ -0,0 +1,9 @@
+const Excel = require('exceljs')
+const filename = process.argv[2] || 'x.csv'
+const outputFile = process.argv[3] || filename.replace(/csv$/i, 'xlsx')
+
+var workbook = new Excel.Workbook();
+workbook.csv.readFile(filename)
+  .then(worksheet => {
+    workbook.xlsx.writeFile(outputFile)
+  });
